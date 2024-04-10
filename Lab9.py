3c1c569ec98beb8f89e23520183f57935d818f47
vrindagoel <vrinda.d.goel@gmail.com>
2024-04-10 17:25:11 -0400

    first commit lol

---
+++ Lab9.py
@@ -0,0 +1,21 @@
+def menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+
+
+def encode():
+    pass
+
+
+def main():
+    while True:
+        menu()
+        selection = int(input("Please enter an option: "))
+
+        if selection == 1:
+            pass
+        elif selection == 2:
+            pass
+        elif selection == 3:
+            pass
+        else:
+            print("Invalid input!")
